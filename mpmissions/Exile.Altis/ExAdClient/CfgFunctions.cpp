cd69bc1dcd31c960f2ce991419c4928fb01121c4
[GADD]Monkeynutz <harry@gamingatdeathsdoor.com>
2018-03-08 22:30:15 +0000

    Update CfgFunctions.cpp

--- mpmissions/Exile.Altis/ExAdClient/CfgFunctions.cpp
+++ mpmissions/Exile.Altis/ExAdClient/CfgFunctions.cpp
@@ -21,10 +21,7 @@ class ExAd
 {
 	tag = "ExAd";
 	#include "Core\CfgFunctions.cpp"
-	//#include "VirtualGarage\CfgFunctions.cpp"
 	//#include "AdminEvents\CfgFunctions.cpp"
-	//#include "Hacking\CfgFunctions.cpp"
-	//#include "Grinding\CfgFunctions.cpp"
 	//#include "HaloParachute\CfgFunctions.cpp"	
 	//#include "XM8\CfgFunctions.cpp"	
 	//#include "StatsBar\CfgFunctions.cpp"	
